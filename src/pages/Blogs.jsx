374cd01365a9c81533e301dc9504385a3a8d743a
aditya-sharma02 <adyashma786@gmail.com>
2023-08-28 23:54:15 +0530

    blog url

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -15,7 +15,7 @@ const Blogs = () => {
         const submit = async () => {
             setloading(true);
             try {
-                const response = await fetch("http://localhost:5000/getblogs")
+                const response = await fetch("https://mental-health-project.onrender.com/getblogs")
                 const data = await response.json()
                 // console.log(data.articles)
                 setdata(data.articles)
