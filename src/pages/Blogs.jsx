eb7e5e8460c3e79d85daeea22abc911d0b986d84
aditya-sharma02 <adyashma786@gmail.com>
2023-08-29 13:28:29 +0530

    comments

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -27,31 +27,6 @@ const Blogs = () => {
         }
         submit();
     }, [])
-    const d1 = {
-        author
-            :
-            "Rick Rojas, Anna Betts",
-        content
-            :
-            "Supporters of the measures have argued that they protect children from medical treatments they see as risky and unproven. On Wednesday, state Rep. Hugh Blackwell, a Republican, said that the laws of … [+3435 chars]",
-        description
-            :
-            "North Carolina became the latest state to block minors from having access to gender-transition care, as Republican lawmakers voted Wednesday to override the governor’s veto of a bill restricting hormone treatments, puberty blockers and surgeries for young peo…",
-        publishedAt
-            :
-            "2023-08-17T01:20:25Z",
-        source
-            :
-            { id: null, name: 'The Boston Globe' },
-        title
-            :
-            "North Carolina bans transgender care for minors as Republicans override veto",
-        url
-            :
-            "https://www.bostonglobe.com/2023/08/16/nation/north-carolina-transgender-affirming-care/",
-        urlToImage
-            : "https://2.bp.blogspot.com/-1DO0SW3US90/ViDunJKlOyI/AAAAAAAA5Wk/1hIjw3VQTx0/s1600/6906500-lake-mountain-landscapes.jpg",
-    }
     if (loading === true) {
         return (
             <>
@@ -64,11 +39,10 @@ const Blogs = () => {
     return (
         <>
             <Navbar />
-            {/* <h1 className="text-5xl text-black mt-28">ldmcldcmdl,</h1> */}
             <div className="w-screen mt-28 flex-col h-auto items-center">
                 {data?.map((elem, id) => {
                     return (
-                        <a key={id} href={d1.url}>
+                        <a key={id} href={elem.url}>
                             <div className="w-[60%] m-auto mt-16 h-auto">
                                 <div className="w-[100%]"><img src={elem.urlToImage} /></div>
                                 <div className="text-2xl font-bold">{elem.title}</div>
